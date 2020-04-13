1a70fec0449352ae12c4e1d9ee859c0680ce878c
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-04-14 01:02:21 +0200

    Print harcoded result for empty string input

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+
+void sha512256(char *input, int inputLength, unsigned long *result) {
+    result[0] = 0xc672b8d1ef56ed28;
+    result[1] = 0xab87c3622c511406;
+    result[2] = 0x9bdd3ad7b8f97374;
+    result[3] = 0x98d0c01ecef0967a;
+}
+
 int main() {
-    printf("Hello, World!\n");
+    char input[] = {""};
+    int inputLength = sizeof(input) / sizeof(typeof(input[0]));
+    unsigned long result[] = {0, 0, 0, 0};
+    sha512256(input, inputLength, result);
+    printf("0x%lx%lx%lx%lx", result[0], result[1], result[2], result[3]);
     return 0;
 }
