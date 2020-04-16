6e35ad08ca0c446fdfbd46ddafbf22a30a8da919
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-04-16 05:45:43 +0200

    Green: toggle a bit

--- test_binary_operations.c
+++ test_binary_operations.c
@@ -5,6 +5,8 @@
 
 static void set_bit(unsigned int *input, unsigned int bitNumber);
 
+static void toggle_bit(unsigned int *pInt, unsigned int bitNumber);
+
 static MunitResult
 set_a_bit(const MunitParameter params[], void *user_data) {
     unsigned int input = 8; // binary: 1000
@@ -26,6 +28,21 @@ set_multiple_bits(const MunitParameter params[], void *user_data) {
 
     munit_assert_ulong (input, ==, expected);
     return MUNIT_OK;
+}
+
+static MunitResult
+toggle_a_bit(const MunitParameter params[], void *user_data) {
+    unsigned int input = 8; // binary: 1000
+    unsigned int expected = 0; // binary: 0
+
+    toggle_bit(&input, 4u);
+
+    munit_assert_ulong (input, ==, expected);
+    return MUNIT_OK;
+}
+
+static void toggle_bit(unsigned int *input, unsigned int bitNumber) {
+    *input ^= 1u << (bitNumber-1);
 }
 
 static void set_bit(unsigned int* input, unsigned int bitNumber) {
