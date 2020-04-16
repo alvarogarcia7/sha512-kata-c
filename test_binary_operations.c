995d2877b2bb4759d475995ba2fd9b1d625b5b75
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-04-16 05:31:47 +0200

    Green: set the bit (scaffold = hardcoded)

--- test_binary_operations.c
+++ test_binary_operations.c
@@ -3,13 +3,20 @@
 #include "munit.h"
 #include <stdlib.h>
 
+static void set_bit(unsigned int *input, int bitNumber);
+
 static MunitResult
 set_a_bit(const MunitParameter params[], void *user_data) {
     unsigned int input = 8; // binary: 1000
     unsigned int expected = 136; // binary: 1000_1000
 
+    set_bit(&input, 8);
 
-//    munit_assert_ulong (input, ==, expected);
+    munit_assert_ulong (input, ==, expected);
     return MUNIT_OK;
+}
+
+static void set_bit(unsigned int* input, int bitNumber) {
+    *input = 136;
 }
 
